e9b137ae90ddd8df56689fcf805c3736bd6e7b4b
faustluck <faustluck91@gmail.com>
2023-03-22 01:12:57 +0300

    init

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.scss";
+import Search from "./components/SearchComponent/Search.jsx";
+import Main from "./components/MainComponent/Main.jsx";
+
+function App() {
+  return (
+    <div className="App">
+      <Search/>
+      <Main/>
+    </div>
+  );
+}
+
+export default App;
