62191605d4b8bc180a848b3a05fbf5c5ca52f856
faustluck <faustluck91@gmail.com>
2023-03-22 14:51:17 +0300

    refactor main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
 import "./index.scss";
 import { Provider } from "react-redux";
 import store from "./store/store.js";
+import Header from "./components/SearchComponent/Header.jsx";
+import Content from "./components/ContentComponent/Content.jsx";
+import Footer from "./components/Footer/Footer.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Provider store={store}>
-      <App/>
+      <Header/>
+      <Content/>
+      <Footer/>
     </Provider>
   </React.StrictMode>,
 );
