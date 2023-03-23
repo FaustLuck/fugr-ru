2bd1bb9afc96da4f6ac686231f62beb17461a25e
faustluck <faustluck91@gmail.com>
2023-03-23 16:26:38 +0300

    Footer.jsx

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,8 +1,19 @@
 import "@c/Footer/Footer.scss";
+import { useDispatch } from "react-redux";
+import { getBooks, updateStartIndex } from "@s/actions.js";
 
 function Footer() {
+  const dispatch = useDispatch();
+
+  function loadMore() {
+    dispatch(updateStartIndex());
+    dispatch(getBooks());
+  }
+
   return (
-    <footer>Футер</footer>
+    <footer>
+      <input type="button" value="Загрузить еще" onClick={loadMore}/>
+    </footer>
   );
 }
 
