c0f23637185dd96c4b8788ab3c32ac3559b83c8a
faustluck <faustluck91@gmail.com>
2023-03-25 20:03:15 +0300

    router file extend

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import "./styles/index.scss";
 import { Provider } from "react-redux";
 import store from "@s/store.js";
 import { RouterProvider } from "react-router-dom";
-import router from "@r/index.jsx";
+import router from "@r/index.js";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
