f75c32e8387a56e64b87c79e1397d13d07f1d1d8
faustluck <faustluck91@gmail.com>
2023-03-26 16:23:42 +0300

    Loader alignment

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -5,10 +5,12 @@ function Loader() {
   const loading = useSelector(state => state.loading);
   if (!loading) return (<></>);
   return (
-    <div className="book">
-      <div className="page static left"></div>
-      <div className="page scrollable"></div>
-      <div className="page static right"></div>
+    <div className="book__wrapper">
+      <div className="book">
+        <div className="page static left"></div>
+        <div className="page scrollable"></div>
+        <div className="page static right"></div>
+      </div>
     </div>
   );
 }
